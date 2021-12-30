17c70054f30a4bf60d5f32e4a296137bd90f7ef6
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2021-12-29 19:25:43 -0600

    UPDATE: Making AJAX

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,16 @@
-import { createSomething } from "./prototyping.js";
+import { fetchingINFO } from "./fetch.js";
+import { xmlGetInfo } from "./XML.js";
 
-const btn = document.querySelector("#btn-create-human");
-btn.addEventListener( "click", createSomething);
+const btnClose = document.querySelector("#close-x");
+const btnfetch = document.querySelector("#fetch");
+const btnXML = document.querySelector("#xml");
+
+window.addEventListener('keyup', function(e) {
+    if( e.key === 'Escape' ) {
+        const box = document.querySelector("#blackbox");
+        box.style.display = "none";
+    }
+})
+
+btnfetch.addEventListener( 'click', fetchingINFO );
+btnXML.addEventListener( 'click', xmlGetInfo );
