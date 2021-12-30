a0dd9249ed3fe00905b15560bd2b30ab6bb7490a
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2021-12-30 09:10:50 -0600

    UPDATE: Web Components Practice

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,1 @@
-import { fetchingINFO } from "./fetch.js";
-import { xmlGetInfo } from "./XML.js";
-
-const btnClose = document.querySelector("#close-x");
-const btnfetch = document.querySelector("#fetch");
-const btnXML = document.querySelector("#xml");
-
-window.addEventListener('keyup', function(e) {
-    if( e.key === 'Escape' ) {
-        const box = document.querySelector("#blackbox");
-        box.style.display = "none";
-    }
-})
-
-btnfetch.addEventListener( 'click', fetchingINFO );
-btnXML.addEventListener( 'click', xmlGetInfo );
+alert()
