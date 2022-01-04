ce386446cfe1773c530b521c7d7523a3ee1775d3
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2022-01-04 09:08:49 -0600

    CREATE: First Typescript file of a bright career

--- javascript/index.js
+++
@@ -1,6 +0,0 @@
-import { Chart } from "./chart.js";
-import { getINFO } from "./apiConsumption.js";
-import { ChartAPI } from './apiChartComponent.js';
-
-window.customElements.define('chart-api', ChartAPI);
-window.customElements.define('chart-jose', Chart);
