57d5bb8da7aacee466d5bc1b0b9ab5f0d8485798
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2022-01-04 09:05:01 -0600

    UPDATE: Prettier

--- javascript/apiChartComponent.js
+++ javascript/apiChartComponent.js
@@ -1,6 +1,6 @@
 import { getINFO } from "./apiConsumption.js";
 
-const template = document.createElement('template');
+const template = document.createElement("template");
 template.innerHTML = `
     <style>
         button {
@@ -16,66 +16,72 @@ template.innerHTML = `
 `;
 
 class ChartAPI extends HTMLElement {
-    constructor () {
-        super();
-        this.attachShadow({mode: "open"});
-        this.shadowRoot.appendChild(template.content.cloneNode(true));
-        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
-    }
-    async getAPI() {
-        const info = await getINFO();
-        console.log(info);
-        const name = info["Meta Data"]["2. Symbol"];
-        
-        const keys = Object.keys(info['Time Series (5min)'])
-        const firstArray = [];
-        
-        for (let index = 0; index < keys.length; index++) {
-           console.log( info['Time Series (5min)'][keys[index]]['2. high'] )
-           console.log( info['Time Series (5min)'][keys[index]]['3. low'] )
-            let creatingArray = [];
-            let phrase = '';
-            phrase += index;
-            phrase += 'hr';
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
+  }
+  async getAPI() {
+    const info = await getINFO();
+    console.log(info);
+    const name = info["Meta Data"]["2. Symbol"];
 
-            if (index === 0) {
-                creatingArray.push("Hour")
-                creatingArray.push('High')
-                creatingArray.push('Low')
-            } else {
-                creatingArray.push( phrase );
-                creatingArray.push( Number(info['Time Series (5min)'][keys[index]]['2. high']) )
-                creatingArray.push( Number(info['Time Series (5min)'][keys[index]]['3. low']) )
-            }
-            
+    const keys = Object.keys(info["Time Series (5min)"]);
+    const firstArray = [];
 
-            firstArray.push( creatingArray );
-        }
+    for (let index = 0; index < keys.length; index++) {
+      console.log(info["Time Series (5min)"][keys[index]]["2. high"]);
+      console.log(info["Time Series (5min)"][keys[index]]["3. low"]);
+      let creatingArray = [];
+      let phrase = "";
+      phrase += index;
+      phrase += "hr";
 
-        console.log(firstArray);
-        
-
-        google.charts.load('current', {'packages':['corechart']});
-        google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable(firstArray);
-
-        var options = {
-          title: 'Company Performance',
-          curveType: 'function',
-          legend: { position: 'bottom' }
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-        chart.draw(data, options);
+      if (index === 0) {
+        creatingArray.push("Hour");
+        creatingArray.push("High");
+        creatingArray.push("Low");
+      } else {
+        creatingArray.push(phrase);
+        creatingArray.push(
+          Number(info["Time Series (5min)"][keys[index]]["2. high"])
+        );
+        creatingArray.push(
+          Number(info["Time Series (5min)"][keys[index]]["3. low"])
+        );
       }
+
+      firstArray.push(creatingArray);
     }
 
-    connectedCallback() {
-        this.shadowRoot.querySelector('button').addEventListener('click', ()=>{ this.getAPI() })
+    console.log(firstArray);
+
+    google.charts.load("current", { packages: ["corechart"] });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable(firstArray);
+
+      var options = {
+        title: "Company Performance",
+        curveType: "function",
+        legend: { position: "bottom" },
+      };
+
+      var chart = new google.visualization.LineChart(
+        document.getElementById("curve_chart")
+      );
+
+      chart.draw(data, options);
     }
+  }
+
+  connectedCallback() {
+    this.shadowRoot.querySelector("button").addEventListener("click", () => {
+      this.getAPI();
+    });
+  }
 }
 
 export { ChartAPI };
