891cfedd5b9a9c88491c1b69eaf2191d36e85286
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2022-01-04 16:08:56 -0600

    UPDATE: Fighting with classes

---
+++ src/classes/person.ts
@@ -0,0 +1,7 @@
+export class Person {
+    constructor( 
+        readonly name: string,
+        public age: number,
+        private job: string
+     ){}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,25 @@
-let mario = "mario";
-let number = 12;
-
-let juan: string;
-let juanito: string = "Juanito";
-
-let functionTest: Function;
-
-functionTest = (): void => {
-    console.log("Mario Kart is an excellent video game");
-}
-
-const say = ():string => 
-{
-    return "Hello World"
-}
-
-const askMeYourName = (name: string, age: number):string => 
-{
-    return `Hello your name is ${name} and your age is ${age}`
-}
+import { Person } from './classes/person.js';
 
 
-let logDetails: ( obj: { name: string, age: number } ) => void;
-
-logDetails = (ninja: { name: string, age: number }) => {
-    console.log(`Your name is ${ninja.name} and is ${ ninja.age }`)
-}
-
+const btn = document.querySelector('#btn-to-create-object') as HTMLButtonElement;
+const name = document.querySelector("#name") as HTMLInputElement;
+let age = document.querySelector('#age') as HTMLInputElement;
+const job = document.querySelector("#job") as HTMLInputElement;
 
 
-logDetails({ name: "Manuel de Dios", age: 31 });
-
-const names: (string|number)[] = [];
-
-names.push("Juan");
-names.push(27);
-
-let giveYourName: (name: string, age: number | string) => void;
-
-giveYourName = (a: string, b: number | string) => {
-    console.log( a, b )
- }
-
-giveYourName("Joselito", 29);
-
-let landingPhrase = document.querySelector('#title') as HTMLParagraphElement;
-console.log(landingPhrase.innerText );
-
-
-class Humanoid {
+class Human {
     name: string;
     age: number;
 
-    constructor(name: string, age: number) {
+    constructor(name: string, age:number) {
         this.name = name;
         this.age = age;
     }
-
-    getName () {
-        return `My name is ${this.name}`
-    }
 }
 
-let juanDeDios = new Humanoid("Juanito", 30);
 
-console.log( juanDeDios.getName() )
-
+btn.addEventListener("click", function(){
+    
+    const onePerson = new Person( name.value, age.valueAsNumber,  job.value )
+    console.log(onePerson);
+});
