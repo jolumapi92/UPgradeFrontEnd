e2f8f8e0f5534185e30a24ac7ffdfe7feaf23f9f
Jose Luis Matias Pineda <jose-luis.matias@atos.net>
2022-01-05 08:37:32 -0600

    UPDATE

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,6 @@ let age = document.querySelector('#age') as HTMLInputElement;
 const job = document.querySelector("#job") as HTMLInputElement;
 
 
-class Human {
-    name: string;
-    age: number;
-
-    constructor(name: string, age:number) {
-        this.name = name;
-        this.age = age;
-    }
-}
-
-
 btn.addEventListener("click", function(){
     
     const onePerson = new Person( name.value, age.valueAsNumber,  job.value )
